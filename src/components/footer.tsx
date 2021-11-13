c7238be6b7549d6ccba314da00ec069fb7f60d48
coolpythoncodes <coolpythoncodes@gmail.com>
2021-11-13 08:38:44 +0100

    footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,10 @@
 const Footer = () => {
     return (
-        <div>
-            <a href="https://github.com/coolpythoncodes/Hang-man-react">Source code</a> | <a href="https://twitter.com/DevRapture">Twiitter</a> | <a href="https://www.linkedin.com/in/rapture-godson/">Linkedln</a> | <a href="https://www.instagram.com/devrapture/">Instagram</a>
-        </div>
+        <footer className="flex justify-center pb-5">
+            <div>
+                <a href="https://github.com/coolpythoncodes/Hang-man-react">Source code</a> | <a href="https://twitter.com/DevRapture">Twiitter</a> | <a href="https://www.linkedin.com/in/rapture-godson/">Linkedln</a> | <a href="https://www.instagram.com/devrapture/">Instagram</a>
+            </div>
+        </footer>
     )
 }
 
